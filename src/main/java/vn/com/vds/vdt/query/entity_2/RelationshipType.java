db4e16cd7033bf6b9c8bec3e133a328298a7ef2b
zeann3th <longhienly112@gmail.com>
2025-08-24 09:23:30 +0700

    feat: schema v2

---
+++ src/main/java/vn/com/vds/vdt/query/entity_2/RelationshipType.java
@@ -0,0 +1,38 @@
+package vn.com.vds.vdt.query.entity_2;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "relationship_types")
+public class RelationshipType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "relationship_type_id")
+    @EqualsAndHashCode.Include
+    private Long relationshipTypeId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "from_entity_type_id", nullable = false)
+    private EntityType fromEntityType;
+
+    @ManyToOne
+    @JoinColumn(name = "to_entity_type_id", nullable = false)
+    private EntityType toEntityType;
+
+    @Column(name = "cardinality", nullable = false)
+    private String cardinality;
+
+    @Column(name = "is_required", nullable = false)
+    private Boolean isRequired;
+}
