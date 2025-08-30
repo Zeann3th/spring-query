681e2710f889c401db8150c371eba073a45f8e95
zeann3th <longhienly112@gmail.com>
2025-08-30 17:10:43 +0700

    feat: migrate v2

--- src/main/java/vn/com/vds/vdt/servicebuilder/controller/CmsController.java
+++ src/main/java/vn/com/vds/vdt/servicebuilder/controller/CmsController.java
@@ -1,24 +1,30 @@
 package vn.com.vds.vdt.servicebuilder.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import vn.com.vds.vdt.servicebuilder.common.base.ResponseWrapper;
 import vn.com.vds.vdt.servicebuilder.controller.dto.parser.ParseSchemaRequest;
 import vn.com.vds.vdt.servicebuilder.service.cms.parser.ParserService;
 
 @SuppressWarnings("all")
 @RestController
-@RequestMapping(value = "/api/v1/cms")
+@RequestMapping(value = "/api")
 @RequiredArgsConstructor
 @ResponseWrapper
 public class CmsController implements ParserService {
     private final ParserService parserService;
 
-    @PostMapping(value = "/migrate")
+    @PostMapping(value = "/v1/cms/migrate")
     public void parse(@RequestBody ParseSchemaRequest request) {
         parserService.parse(request);
+    }
+
+    @PostMapping(value = "/v2/cms/migrate")
+    public void parseV2(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("type") String type
+    ) {
+        parserService.parseV2(file, type);
     }
 }
--- src/main/java/vn/com/vds/vdt/servicebuilder/service/cms/parser/ParserService.java
+++ src/main/java/vn/com/vds/vdt/servicebuilder/service/cms/parser/ParserService.java
@@ -1,7 +1,10 @@
 package vn.com.vds.vdt.servicebuilder.service.cms.parser;
 
+import org.springframework.web.multipart.MultipartFile;
 import vn.com.vds.vdt.servicebuilder.controller.dto.parser.ParseSchemaRequest;
 
 public interface ParserService {
     void parse(ParseSchemaRequest request);
+
+    void parseV2(MultipartFile file, String type);
 }
